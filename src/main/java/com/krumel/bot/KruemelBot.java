ca356659987fce4bb205db2b3920cd7eebe0088a
Filip M <filipmilosevic2001@gmail.com>
2018-08-13 21:26:43 +0200

    Implement EventListener

--- src/main/java/com/krumel/bot/KruemelBot.java
+++ src/main/java/com/krumel/bot/KruemelBot.java
@@ -1,4 +1,37 @@
 package com.krumel.bot;
 
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.EventListener;
+
+import javax.security.auth.login.LoginException;
+
+
 public class KruemelBot {
+
+    public void main(String[] args) {
+
+
+
+    }
+
+    private class BotSetup implements EventListener {
+
+        public void SetupBot()
+        throws LoginException, RateLimitedException, InterruptedException{
+
+
+
+        }
+
+        @Override
+        public void onEvent(Event event) {
+
+
+
+        }
+
+    }
+
 }
+
