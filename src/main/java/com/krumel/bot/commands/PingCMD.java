b55debd5753369952fe91fa6f250a994a92cb7c5
ZickZackHD <lukicrafting@gmail.com>
2018-09-22 20:05:48 +0200

    #Add PingCMD

---
+++ src/main/java/com/krumel/bot/commands/PingCMD.java
@@ -0,0 +1,37 @@
+
+        package com.krumel.bot.commands;
+
+        import java.time.temporal.ChronoUnit;
+        import com.jagrosh.jdautilities.command.Command;
+        import com.jagrosh.jdautilities.command.CommandEvent;
+        import com.jagrosh.jdautilities.doc.standard.CommandInfo;
+        import com.jagrosh.jdautilities.examples.doc.Author;
+
+/**
+ *
+ * @author John Grosh (jagrosh)
+ */
+@CommandInfo(
+        name = {"Ping", "Pong"},
+        description = "Checks the bot's latency"
+)
+@Author("John Grosh (jagrosh)")
+public class PingCMD extends Command {
+
+    public PingCMD()
+    {
+        this.name = "ping";
+        this.help = "checks the bot's latency";
+        this.guildOnly = false;
+        this.aliases = new String[]{"pong"};
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        event.reply("Ping: ...", m -> {
+            long ping = event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS);
+            m.editMessage("Ping: " + ping  + "ms | Websocket: " + event.getJDA().getPing() + "ms").queue();
+        });
+    }
+
+}
--- src/main/java/com/krumel/bot/commands/ShutdownCMD.java
+++ src/main/java/com/krumel/bot/commands/ShutdownCMD.java
@@ -1,4 +1,7 @@
 package com.krumel.bot.commands;
 
 public class ShutdownCMD {
+
+
+
 }
