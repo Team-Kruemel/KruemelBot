c457dd92a34771be493dc56025e5b8bc1dfecbd8
ZickZackHD <lukicrafting@gmail.com>
2018-09-22 20:39:18 +0200

    #Add Coinflip

--- src/main/java/com/krumel/bot/commands/CoinflipCMD.java
+++ src/main/java/com/krumel/bot/commands/CoinflipCMD.java
@@ -1,10 +1,14 @@
-package com.krumel.bot;
+package com.krumel.bot.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.doc.standard.CommandInfo;
 import com.jagrosh.jdautilities.examples.doc.Author;
-public class DdosCMD {
+
+import java.util.Random;
+
+public class CoinflipCMD {
+
     @CommandInfo(
             name = {"Ddos", "ddos"},
             description = "start ddos!"
@@ -22,12 +26,17 @@ public class DdosCMD {
 
         @Override
         protected void execute(CommandEvent event) {
-            event.getTextChannel().sendMessage("https://www.heinz-lukas.de/ddos/").queue();
 
 
-            }
+            int  IRandom = new Random().nextInt(1);
+            if(IRandom==0) {
+                event.getTextChannel().sendMessage("Head!\uD83D\uDE03").queue();
+
+            }else
+            event.getTextChannel().sendMessage("Number 0").queue();
+
+
         }
-
     }
 
-
+}
