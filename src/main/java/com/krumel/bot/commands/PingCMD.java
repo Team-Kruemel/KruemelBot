496887056826a81d6959b33bdf06041e8d72758b
ZickZackHD <lukicrafting@gmail.com>
2018-10-16 20:36:27 +0200

    rework PingCMD but won't work

--- src/main/java/com/krumel/bot/commands/PingCMD.java
+++ src/main/java/com/krumel/bot/commands/PingCMD.java
@@ -6,6 +6,10 @@
         import com.jagrosh.jdautilities.command.CommandEvent;
         import com.jagrosh.jdautilities.doc.standard.CommandInfo;
         import com.jagrosh.jdautilities.examples.doc.Author;
+        import net.dv8tion.jda.client.managers.EmoteManager;
+        import net.dv8tion.jda.core.EmbedBuilder;
+        import net.dv8tion.jda.core.entities.Emote;
+        import net.dv8tion.jda.client.*;
         /*
          *
          *
@@ -37,10 +41,47 @@ public class PingCMD extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
+
+
+        EmbedBuilder eb = new EmbedBuilder();
+        final String SBOT_URL = "https://github.com/Team-Kruemel/KruemelBot/";
+        final String SPING_TITLE = "Here is your invite-Link!";
+        long PINGMS = 0;
+        final String SNoPerm = "You don't have permission to use this command!";
+        final String SFOOTER_TEXT = "Requested by " + event.getAuthor().getName();
         event.reply("Ping: ...", m -> {
             long ping = event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS);
-            m.editMessage("Ping: " + ping  + "ms | Websocket: " + event.getJDA().getPing() + "ms").queue();
+
+
+
+
+
+
         });
+        final  String SDESC = "Ping: " + PINGMS + "ms | Websocket: " +  event.getJDA().getPing() + "ms";
+
+        if (event.getAuthor().isBot()) {
+
+            return;
+
+        } else {
+
+
+
+            // Prepare Embed Message
+            eb.setAuthor(event.getSelfUser().getName(), SBOT_URL, event.getSelfUser().getAvatarUrl());
+            eb.setTitle(SPING_TITLE);
+            eb.setDescription(SDESC);
+            eb.setFooter(SFOOTER_TEXT, event.getAuthor().getAvatarUrl());
+            //send The Invite link
+
+            event.getTextChannel().sendMessage(eb.build()).queue();
+        }
+
     }
 
-}
+
+
+    }
+
+
